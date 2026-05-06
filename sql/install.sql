bad652cde11e7a06150d4d1bfadc618632e68966
Maria Eleftheria Angeletopoulou <angeletopoulou.m@gmail.com>
2026-05-06 17:44:39 +0300

    added DROP TABLE IF EXISTS to all tables, added more foreign keys to the On_Duty table (though the table structure is very likely not correct)

--- sql/install.sql
+++ sql/install.sql
@@ -2,8 +2,10 @@ DROP SCHEMA IF EXISTS Ygeiopolis-Management;
 CREATE SCHEMA Ygeiopolis-Management;
 USE Ygeiopolis-Management;
 
+DROP TABLE IF EXISTS `Staff`;
+
 CREATE TABLE IF NOT EXISTS `Staff` (
-    `amka` BIGINT(11) NOT NULL UNIQUE,
+    `amka` VARCHAR(11) NOT NULL UNIQUE,
     `first_name` VARCHAR(45) NOT NULL,
     `last_name` VARCHAR(45) NOT NULL,
     `date_of_birth` DATE NOT NULL,
@@ -14,20 +16,24 @@ CREATE TABLE IF NOT EXISTS `Staff` (
     PRIMARY KEY (`amka`)
 );
 
+DROP TABLE IF EXISTS `Doctor`;
+
 CREATE TABLE IF NOT EXISTS `Doctor` (
-    `amka` BIGINT(11) NOT NULL UNIQUE,
+    `amka` VARCHAR(11) NOT NULL UNIQUE,
     `license_number` INT NOT NULL UNIQUE,
     `specialty` VARCHAR(45) NOT NULL,
     `rank_level` VARCHAR(45) NOT NULL,
     `monthly_shifts_worked` INT NOT NULL,
     `consecutive_shifts` INT NOT NULL,
-    `supervisor_amka` INT,
+    `supervisor_amka` VARCHAR(11),
     CONSTRAINT `fk_doctor_amka` FOREIGN KEY (`amka`) REFERENCES `Staff` (`amka`) ON DELETE CASCADE,
     CONSTRAINT `fk_doctor_supervisor` FOREIGN KEY (`supervisor_amka`) REFERENCES `Doctor` (`amka`) ON DELETE SET NULL
 );
 
+DROP TABLE IF EXISTS `Nurse`;
+
 CREATE TABLE IF NOT EXISTS `Nurse` (
-    `amka` BIGINT(11) NOT NULL UNIQUE,
+    `amka` VARCHAR(11) NOT NULL UNIQUE,
     `rank` VARCHAR(45) NOT NULL,
     `department_code` INT NOT NULL,
     `monthly_shifts_worked` INT NOT NULL,
@@ -36,8 +42,10 @@ CREATE TABLE IF NOT EXISTS `Nurse` (
     CONSTRAINT `fk_nurse_department` FOREIGN KEY (`department_code`) REFERENCES `Department` (`department_code`)
 );
 
+DROP TABLE IF EXISTS `Administrative_staff`;
+
 CREATE TABLE IF NOT EXISTS `Administrative_staff` (
-    `amka` BIGINT(11) NOT NULL UNIQUE,
+    `amka` VARCHAR(11) NOT NULL UNIQUE,
     `position` VARCHAR(45) NOT NULL,
     `office` VARCHAR(45) NOT NULL,
     `department_code` INT NOT NULL,
@@ -47,16 +55,20 @@ CREATE TABLE IF NOT EXISTS `Administrative_staff` (
     CONSTRAINT `fk_admin_department` FOREIGN KEY (`department_code`) REFERENCES `Department` (`department_code`)
 );
 
+DROP TABLE IF EXISTS `Department`;
+
 CREATE TABLE IF NOT EXISTS `Department` (
     `department_code` INT NOT NULL UNIQUE,
     `name` VARCHAR(45) NOT NULL UNIQUE,
     `description` VARCHAR(255) NOT NULL,
     `number_of_beds` INT NOT NULL,
     `building_floor` VARCHAR(45) NOT NULL,
-    `department_head` INT(12) NOT NULL UNIQUE,
+    `department_head` VARCHAR(11) NOT NULL UNIQUE,
     CONSTRAINT `fk_department_head` FOREIGN KEY (`department_head`) REFERENCES `Doctor` (`amka`) ON DELETE SET NULL,
     PRIMARY KEY (`department_code`)
 );
+
+DROP TABLE IF EXISTS `Beds`;
 
 CREATE TABLE IF NOT EXISTS `Beds` (
     `id_number` INT NOT NULL UNIQUE AUTO_INCREMENT,
@@ -67,8 +79,10 @@ CREATE TABLE IF NOT EXISTS `Beds` (
     CONSTRAINT `fk_beds_department` FOREIGN KEY (`department_code`) REFERENCES `Department` (`department_code`) ON DELETE CASCADE
 );
 
+DROP TABLE IF EXISTS `Hospitilization`;
+
 CREATE TABLE IF NOT EXISTS `Hospitilization` (
-    `amka` BIGINT(11) NOT NULL,
+    `amka` VARCHAR(11) NOT NULL,
     `department_code` INT NOT NULL,
     `bed_id_number` INT NOT NULL UNIQUE,
     `admission_date` DATE NOT NULL,
@@ -83,8 +97,10 @@ CREATE TABLE IF NOT EXISTS `Hospitilization` (
     CONSTRAINT `fk_hospitilization_bed` FOREIGN KEY (`bed_id_number`) REFERENCES `Beds` (`id_number`)
 );
 
+DROP TABLE IF EXISTS `Patient`;
+
 CREATE TABLE IF NOT EXISTS `Patient` (
-    `amka` BIGINT(11) NOT NULL UNIQUE,
+    `amka` VARCHAR(11) NOT NULL UNIQUE,
     `first_name` VARCHAR(45) NOT NULL,
     `last_name` VARCHAR(45) NOT NULL,
     `fathers_name` VARCHAR(45) NOT NULL,
@@ -104,6 +120,70 @@ CREATE TABLE IF NOT EXISTS `Patient` (
     PRIMARY KEY (`amka`)
 );
 
+DROP TABLE IF EXISTS `Medicine`;
+
 CREATE TABLE IF NOT EXISTS `Medicine`(
-    `prescribed_medicine`
+    `medicine_name` VARCHAR(255) NOT NULL UNIQUE,
+    `active_substance` VARCHAR(255) NOT NULL,
+    `route_of_administration` VARCHAR(255) NOT NULL,
+    `product_autorization_country` VARCHAR(255) NOT NULL,
+    `marketing_authorization_holder` VARCHAR(255) NOT NULL,
+    `pharmacovigilance_system_master_file_location` VARCHAR(255) NOT NULL,
+    `pharmacovigilance_enquires_email_address` VARCHAR(255) NOT NULL,
+    `pharmacovigilance_enquires_phone_number` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`medicine_name`, `active_substance`)
+);
+
+DROP TABLE IF EXISTS `Prescription`;
+
+CREATE TABLE IF NOT EXISTS `Prescription` (
+    `id` VARCHAR(255) NOT NULL UNIQUE AUTO_INCREMENT,
+    `dosage` VARCHAR(45) NOT NULL,
+    `frequency` VARCHAR(45) NOT NULL,
+    `start_date` DATE NOT NULL UNIQUE,
+    `end_date` DATE,
+    `patient_amka` VARCHAR(11) NOT NULL UNIQUE,
+    `doctor_amka` VARCHAR(11) NOT NULL UNIQUE,
+    `medicine_name` VARCHAR(255) NOT NULL UNIQUE,
+    `medicine_active_substance` VARCHAR(255) NOT NULL UNIQUE,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `fk_prescription_amka` FOREIGN KEY (`patient_amka`) REFERENCES `Patient` (`amka`) ON DELETE CASCADE,
+    CONSTRAINT `fk_prescription_medicine` FOREIGN KEY (`medicine_name`, `medicine_active_substance`) REFERENCES `Medicine` (`medicine_name`, `active_substance`)
+    CONSTRAINT `fk_prescription_doctor` FOREIGN KEY (`doctor_amka`) REFERENCES `Doctor` (`amka`)
+);
+
+DROP TABLE IF EXISTS `Cost_Calculation`;
+
+CREATE TABLE IF NOT EXISTS `Cost_Calculation` (
+    `KEN` VARCHAR(5) NOT NULL,
+    `base_cost` INT NOT NULL,
+    `MDN` INT NOT NULL,    
+    `patient_amka` VARCHAR(11) NOT NULL UNIQUE,
+    `total_cost` DECIMAL(10,2) NOT NULL,
+    PRIMARY KEY (`KEN`),
+    CONSTRAINT `fk_cost_calculation_amka` FOREIGN KEY (`patient_amka`) REFERENCES `Patient` (`amka`) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS `Insurance_Type`;
+
+CREATE TABLE IF NOT EXISTS `Insurance_Type`(
+    `patient_amka` VARCHAR(11) NOT NULL UNIQUE,
+    `insurance_provider` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`patient_amka`),
+    CONSTRAINT `fk_insurance_type_amka` FOREIGN KEY (`patient_amka`) REFERENCES `Patient` (`amka`) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS `On_Duty`;
+
+CREATE TABLE IF NOT EXISTS `On_Duty` (
+    `department_code` INT NOT NULL,
+    `administrative_staff_amka` VARCHAR(11) NOT NULL UNIQUE,
+    `nurse_amka` VARCHAR(11) NOT NULL UNIQUE,
+    `doctor_amka` VARCHAR(11) NOT NULL UNIQUE,
+    `date` DATE NOT NULL,
+    `shift` VARCHAR(45) NOT NULL,
+    PRIMARY KEY (`administrative_staff_amka`, `date`, `shift`),
+    CONSTRAINT `fk_on_duty_administrative_staff_amka` FOREIGN KEY (`administrative_staff_amka`) REFERENCES `Administrative_staff` (`amka`) ON DELETE CASCADE,
+    CONSTRAINT `fk_on_duty_nurse_amka` FOREIGN KEY (`nurse_amka`) REFERENCES `Nurse` (`amka`) ON DELETE CASCADE,
+    CONSTRAINT `fk_on_duty_doctor_amka` FOREIGN KEY (`doctor_amka`) REFERENCES `Doctor` (`amka`) ON DELETE CASCADE
 );
