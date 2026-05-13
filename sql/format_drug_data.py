57b7962549885325383e5a5914c0c084df36b71c
Maria Eleftheria Angeletopoulou <angeletopoulou.m@gmail.com>
2026-05-13 20:04:24 +0300

    Changed the path in the python script, adding properly formatted data for medicine

--- sql/format_drug_data.py
+++ sql/format_drug_data.py
@@ -1,6 +1,6 @@
 import pandas as pd
-csv_input_path = 'C:/Users/malli/OneDrive/Desktop/ΣΗΜΜΥ/3o_etos/databases/Hospital-data-management/formatted-data/medicine-ema-codes/article-57-product-data_en.csv'
-csv_output_path = 'C:/Users/malli/OneDrive/Desktop/ΣΗΜΜΥ/3o_etos/databases/Hospital-data-management/formatted-data/medicine-ema-codes/formated_article-57-product-data_en.csv'
+csv_input_path = 'C:/Users/maril/Desktop/DBLab/Hospital-data-management/formatted-data/medicine-ema-codes/med-data.csv'
+csv_output_path = 'C:/Users/maril/Desktop/DBLab/Hospital-data-management/formatted-data/medicine-ema-codes/formatted-med-data.csv'
 dataframe = pd.read_csv(csv_input_path, header = None, delimiter=';', names=['product_name','active_substance','route','country', 'marketing', 'master_file_location', 'email', 'phone'])
 dataframe = dataframe.assign(active_substance=dataframe['active_substance'].str.split(',')).explode('active_substance')
 dataframe = dataframe.assign(active_substance=dataframe['active_substance'].str.split('|')).explode('active_substance')
