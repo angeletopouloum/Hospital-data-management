f0f4f0bcb5f8b96e79884938b1b4eac9f276684a
raniamalli <mallirania.com>
2026-05-14 14:02:04 +0300

    added department table, put NULL in head of department and 250 (max) beds in bed count

--- sql/load.sql
+++ sql/load.sql
@@ -1,1 +1,32 @@
 
+-- INSERTS FOR DEPARTMENT
+INSERT INTO Department (department_code, name, description, number_of_beds, building_floor, building, department_head_AMKA) 
+VALUES (66, 'Emergency department', 'Specializes in emergency medical care', 250, 3, 'Emergency department Building', NULL);
+INSERT INTO Department (department_code, name, description, number_of_beds, building_floor, building, department_head_AMKA) 
+VALUES (53, 'Intensive care unit', 'Specializes in ICU', 250, 4, 'Intensive care unit Building', NULL);
+INSERT INTO Department (department_code, name, description, number_of_beds, building_floor, building, department_head_AMKA) 
+VALUES (16, 'Medical-surgical', 'Specializes in medical-surgical care', 250, 4, 'Medical-surgical Building', NULL);
+INSERT INTO Department (department_code, name, description, number_of_beds, building_floor, building, department_head_AMKA) 
+VALUES (36, 'Pediatrics', 'Specializes in pediatric care', 250, 1, 'Pediatrics Building', NULL);
+INSERT INTO Department (department_code, name, description, number_of_beds, building_floor, building, department_head_AMKA) 
+VALUES (96, 'Oncology', 'Specializes in oncology care', 250, 4, 'Oncology Building', NULL);
+INSERT INTO Department (department_code, name, description, number_of_beds, building_floor, building, department_head_AMKA) 
+VALUES (35, 'Cardiology', 'Specializes in cardiology care', 250, 4, 'Cardiology Building', NULL);
+INSERT INTO Department (department_code, name, description, number_of_beds, building_floor, building, department_head_AMKA)
+ VALUES (10, 'Radiology', 'Specializes in radiology care', 250, 3, 'Radiology Building', NULL);
+INSERT INTO Department (department_code, name, description, number_of_beds, building_floor, building, department_head_AMKA) 
+VALUES (65, 'Laboratory services', 'Specializes in laboratory services', 250, 3, 'Laboratory services Building', NULL);
+INSERT INTO Department (department_code, name, description, number_of_beds, building_floor, building, department_head_AMKA) 
+VALUES (62, 'Pharmacy', 'Specializes in pharmacy care', 250, 4, 'Pharmacy Building', NULL);
+INSERT INTO Department (department_code, name, description, number_of_beds, building_floor, building, department_head_AMKA) 
+VALUES (22, 'Rehabilitation', 'Specializes in rehabilitation care', 250, 3, 'Rehabilitation Building', NULL);
+INSERT INTO Department (department_code, name, description, number_of_beds, building_floor, building, department_head_AMKA) 
+VALUES (68, 'Psychiatry', 'Specializes in psychiatry care', 250, 3, 'Psychiatry Building', NULL);
+INSERT INTO Department (department_code, name, description, number_of_beds, building_floor, building, department_head_AMKA) 
+VALUES (92, 'Dermatology', 'Specializes in dermatology care', 250, 3, 'Dermatology Building', NULL);
+INSERT INTO Department (department_code, name, description, number_of_beds, building_floor, building, department_head_AMKA) 
+VALUES (40, 'Gynaecology', 'Specializes in gynaecology care', 250, 1, 'Gynaecology Building', NULL);
+INSERT INTO Department (department_code, name, description, number_of_beds, building_floor, building, department_head_AMKA) 
+VALUES (1, 'Pathology', 'Specializes in pathology care', 250, 1, 'Pathology Building', NULL);
+INSERT INTO Department (department_code, name, description, number_of_beds, building_floor, building, department_head_AMKA) 
+VALUES (58, 'Ophthalmology', 'Specializes in ophthalmology care', 250, 2, 'Ophthalmology Building', NULL);
